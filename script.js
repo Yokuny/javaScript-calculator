5ddeaf28302190959b4223c6dad0458be2195f0f
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-11-17 09:45:14 -0300

    first equation working
    division equation is done and working

--- script.js
+++ script.js
@@ -4,11 +4,29 @@ const operatorDisplay = document.getElementById("operatorDisplay");
 let firstValueSequence = [];
 let secondValueSequence = [];
 let operator;
-function operate(firstValue, operator, secondValue){
+function operate(){
+    historyDisplay.innerHTML = '';
+    typingDisplay.innerHTML = '';
+    operatorDisplay.innerHTML = '';
+    operatorDisplay.textContent = "=";
+    firstValueSequence = firstValueSequence.join('');
+    console.log(`1º valor: ${firstValueSequence}`);
+    console.log(`2º valor: ${secondValueSequence}`);
+    historyDisplay.textContent = `${secondValueSequence} ${operator} ${firstValueSequence}`;
 
+    let equationResult = 0;
+    console.log(operator);
+    switch (operator) {
+        case "/":
+            equationResult = parseFloat(secondValueSequence) / parseFloat(firstValueSequence);
+            typingDisplay.textContent = equationResult;
+            break;
+        default:
+            break;
+    }
 }
 function historyDisplayRender(){
-    historyDisplay.textContent = secondValueSequence.join('');
+    historyDisplay.textContent = secondValueSequence;
 }
 function eraseMemory(){
     firstValueSequence.length = 0
@@ -22,21 +40,30 @@ function eraseLastValue(){
     firstValueSequence.pop();
     typingDisplay.textContent = firstValueSequence.join('');
 }
+
+
 function firstDisplay(value){
+    console.log(value);
+
+
     if(typeof(value) == 'string'){
+        console.log("processo de string");
+
+
+        operator = value;
         if(firstValueSequence.length != 0){
-            secondValueSequence = firstValueSequence;
+            secondValueSequence = firstValueSequence.join('');
             historyDisplayRender();
             firstValueSequence.length = 0
             typingDisplay.textContent = '';
             operatorDisplay.textContent = value;
         }
     }else{
+        console.log("processo de valor");
+
         firstValueSequence.push(value);
         typingDisplay.textContent = firstValueSequence.join('');
     }
+    console.log(`valor 1ª: ${firstValueSequence.join('')} valor 2ª: ${secondValueSequence}`);
 
-    //armazenar o valor em uma variavel para depois calcula-los
-    //pegar o operador selecionado
-    //e calcular quando o = por acionado
 }
