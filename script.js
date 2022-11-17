b0128bc193c3c845d443c0ee9f594f813fbdecd6
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2022-11-16 21:16:28 -0300

    more two functionss implemented
    erase the last value the user insert. and a erase memory of the calculator

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const historyDisplay = document.getElementById("secondDisplay");
 const typingDisplay = document.getElementById("firstDisplay");
+const operatorDisplay = document.getElementById("operatorDisplay");
 let firstValueSequence = [];
 let secondValueSequence = [];
 let operator;
@@ -9,13 +10,27 @@ function operate(firstValue, operator, secondValue){
 function historyDisplayRender(){
     historyDisplay.textContent = secondValueSequence.join('');
 }
+function eraseMemory(){
+    firstValueSequence.length = 0
+    secondValueSequence.length = 0
+    operator = '';
+    historyDisplay.innerHTML = '';
+    typingDisplay.innerHTML = '';
+    operatorDisplay.innerHTML = '';
+}
+function eraseLastValue(){
+    firstValueSequence.pop();
+    typingDisplay.textContent = firstValueSequence.join('');
+}
 function firstDisplay(value){
-    console.log(value);
     if(typeof(value) == 'string'){
-        secondValueSequence = firstValueSequence;
-        historyDisplayRender();
-        firstValueSequence.length = 0
-        typingDisplay.textContent = '';
+        if(firstValueSequence.length != 0){
+            secondValueSequence = firstValueSequence;
+            historyDisplayRender();
+            firstValueSequence.length = 0
+            typingDisplay.textContent = '';
+            operatorDisplay.textContent = value;
+        }
     }else{
         firstValueSequence.push(value);
         typingDisplay.textContent = firstValueSequence.join('');
