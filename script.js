f9f07090cc5f5a2ee377759055c02966a4db2f4c
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-01-10 15:38:42 -0300

    bug fix

--- script.js
+++ script.js
@@ -1,70 +1,64 @@
 const historyDisplay = document.getElementById("secondDisplay");
-const typingDisplay = document.getElementById("firstDisplay");
+const display = document.getElementById("firstDisplay");
 const operatorDisplay = document.getElementById("operatorDisplay");
-let firstValueSequence = [];
-let secondValueSequence = 0;
-let equationResult;
+let displayValue = [];
+let memoryValue = 0;
+let finalSum;
 let operator;
 
 const core = (() => {
   const ans = (value) => {
-    //pego todos os valores recebido do usuario e passo para uma variavel armazenar
-    secondValueSequence = value;
-    //e limpo a primeira variavel para voltar a salvar os proximos numeros
-    firstValueSequence.length = 0;
+    memoryValue = value;
+    displayValue.length = 0;
     historyDisplay.textContent = value;
-    typingDisplay.textContent = "";
+    display.textContent = "";
     operatorDisplay.textContent = operator;
   };
   const eraseMemory = (all) => {
-    //apago alguns display e reseto algumas variaveis
     operatorDisplay.textContent = "=";
-    firstValueSequence.length = 0;
-    typingDisplay.textContent = equationResult;
-    //zero todos meus displays e todas variaveis caso all = true
+    displayValue.length = 0;
+    display.textContent = finalSum;
     if (all) {
-      secondValueSequence = 0;
+      memoryValue = 0;
       operator = "";
-      equationResult = undefined;
+      finalSum = undefined;
       operatorDisplay.innerHTML = "";
-      typingDisplay.innerHTML = "";
+      display.innerHTML = "";
       historyDisplay.innerHTML = "";
     }
   };
   const eraseLastValue = () => {
-    //apaga o ultimo valor da array com pop e mostra no display
-    firstValueSequence.pop();
-    typingDisplay.textContent = firstValueSequence.join("");
+    displayValue.pop();
+    display.textContent = displayValue.join("");
   };
-  const sum = (a, b) => a + b;
-  const subtraction = (a, b) => a - b;
-  const multiplication = (a, b) => a * b;
-  const division = (a, b) => a / b;
+  function sum(a, b) {
+    return a + b;
+  }
+  function subtraction(a, b) {
+    return a - b;
+  }
+  function multiplication(a, b) {
+    return a * b;
+  }
+  function division(a, b) {
+    return a / b;
+  }
+
   const operate = () => {
-    historyDisplay.textContent = `${secondValueSequence} ${operator} ${firstValueSequence.join(
-      ""
-    )}`;
+    historyDisplay.textContent = `${memoryValue} ${operator} ${displayValue.join("")}`;
+    console.log(operator);
     switch (operator) {
       case "/":
-        equationResult = division(
-          secondValueSequence / parseInt(firstValueSequence.join(""))
-        );
+        finalSum = division(memoryValue, parseInt(displayValue.join("")));
         break;
       case "*":
-        equationResult = multiplication(
-          secondValueSequence * parseInt(firstValueSequence.join(""))
-        );
+        finalSum = multiplication(memoryValue, parseInt(displayValue.join("")));
         break;
       case "-":
-        z;
-        equationResult = subtraction(
-          secondValueSequence - parseInt(firstValueSequence.join(""))
-        );
+        finalSum = subtraction(memoryValue, parseInt(displayValue.join("")));
         break;
       case "+":
-        equationResult = sum(
-          secondValueSequence + parseInt(firstValueSequence.join(""))
-        );
+        finalSum = sum(memoryValue, parseInt(displayValue.join("")));
         break;
       default:
         break;
@@ -75,20 +69,15 @@ const core = (() => {
 })();
 
 function firstDisplay(value) {
-  //essa função imprime na primeira tela os digitos que vão sendo digitados
-  //caso seja passado um operador. Para a recepção de numero na primeira variavel
   if (typeof value == "string") {
-    //pego o operador em uma variavel
-    operator = value;
-    //confiro se o usuario esta tentando calcular sem ter passado algum numero previamente
-    if (firstValueSequence.length != 0) {
-      core.ans(parseInt(firstValueSequence.join("")));
-    } else if (equationResult != undefined) {
-      core.ans(equationResult);
+    if (displayValue.length != 0) {
+      operator = value;
+      core.ans(parseInt(displayValue.join("")));
+    } else if (finalSum != undefined) {
+      core.ans(finalSum);
     }
   } else {
-    //caso seja um numero eu o salvo em um array de valores
-    firstValueSequence.push(value);
-    typingDisplay.textContent = firstValueSequence.join("");
+    displayValue.push(value);
+    display.textContent = displayValue.join("");
   }
 }
